6d1bcc2b9e4db88ccbc4d65fb04e32b038c9a8d3
Daniel-Alves-Projects <contato@danielalves.tec.br>
2026-03-08 13:54:02 -0300

    django admin

--- core/models.py
+++ core/models.py
@@ -5,8 +5,13 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
     estoque = models.IntegerField('Quantidade em Estoque')
     
+    def __str__(self):
+        return self.nome
+    
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
     sobrenome = models.CharField('Sobrenome', max_length=100)
     email = models.EmailField('E-mail', max_length=100)
-    
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
