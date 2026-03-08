a8a52b268d0dc9173ebc29fe91d2942c63e3780c
Daniel-Alves-Projects <contato@danielalves.tec.br>
2026-03-08 12:12:28 -0300

    make migrations

--- core/models.py
+++ core/models.py
@@ -5,4 +5,8 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
     estoque = models.IntegerField('Quantidade em Estoque')
     
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
     
